893a550dff7252ba6e67a5ffc42e91b43d821be0
Niraj1903 <niraj.singh1903@outlook.com>
2025-10-28 18:27:54 +0530

    pending , completed and all task sorting logic using useMemo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const Todo = () => {
   const [input, setInput] = useState("");
@@ -30,6 +30,10 @@ const Todo = () => {
       })
     );
   };
+
+  const pending = useMemo(() => todo.filter((p) => !p.completed), [todo]);
+  const completed = useMemo(() => todo.filter((c) => c.completed), [todo]);
+  const all = useMemo(() => todo, [todo]);
   return (
     <>
       <div>
@@ -52,12 +56,34 @@ const Todo = () => {
                 onChange={() => handleToggle(i.id)}
                 checked={i.completed}
               />
-              <span>{i.text}</span>
+              <span className={i.completed ? "line-through" : ""}>
+                {i.text}
+              </span>
               <button onClick={() => handleDelete(i.id)}>Delete</button>
             </li>
           ))}
         </ul>
       </div>
+      <h6>PENDING</h6>
+      <ul>
+        {pending.map((p) => (
+          <li key={p.id}>{p.text}</li>
+        ))}
+      </ul>
+
+      <h6>COMPLETED</h6>
+      <ul>
+        {completed.map((c) => (
+          <li key={c.id}>{c.text}</li>
+        ))}
+      </ul>
+
+      <h6>ALL TASK</h6>
+      <ul>
+        {all.map((a) => (
+          <li key={a.id}>{a.text}</li>
+        ))}
+      </ul>
     </>
   );
 };
